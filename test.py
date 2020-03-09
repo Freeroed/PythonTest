ee1f7e54c5b436b0db75e49286e62b618dfb5835
Freeroed <telekines1551@gmail.com>
2020-03-09 19:49:00 +0300

    Двидения бота и отскок от ракетки

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ window = pygame.display.set_mode((win_W,win_H))
 # Настройки для шарика
 ball_x = 200
 ball_y = 200
-speed_x = 4
+speed_x = 15
 speed_y = 3
 ball_R=30
 #Настройки для ракетки игрока
@@ -19,6 +19,14 @@ rect_x=0
 rect_y=0
 rect_W=30
 rect_H=150
+player_speed=20
+
+#Настройка ракетки бота
+bot_rect_x = win_W-rect_W
+bot_rect_y = 0
+bot_speed = 5
+
+
 
 # Основной цикл игры
 while Run:
@@ -26,7 +34,7 @@ while Run:
     clock.tick(30)
     # Заливка фона определённым цветом.
     # Цвет указывается в модели RGB(R,G,B), где насыщенность цвета указватся в пределах 0-255
-    window.fill((0,40,50))
+    window.fill((255,255,255))
     # Получение массива всех нажатых клавиш
     keys=pygame.key.get_pressed()
     #Основной цикл обработки игровых событий
@@ -42,22 +50,42 @@ while Run:
                 speed_y+= 3
     #Обработка нажатия клавиш. Сравнение значения элемента массива с названием клавиши и выполнение соответствующего действия
     if keys[pygame.K_DOWN] and not(rect_y+rect_H >= win_H):
-        rect_y+=20
+        rect_y+=player_speed
     if keys[pygame.K_UP] and not(rect_y <= 0):
-        rect_y-=20
+        rect_y-=player_speed
     
+    #Движения бота за шариком
+    if (bot_rect_y+(rect_H/2) < ball_y and bot_rect_y + rect_H <= win_H):
+        bot_rect_y+=bot_speed
+    if (bot_rect_y+(rect_H/2) > ball_y and bot_rect_y >= 0):
+        bot_rect_y-=bot_speed
     # Отрисовка круга pygame.draw.circle(Окно, на котором надо отрисовать, (цвет), (Координаты центра), радиус)
     pygame.draw.circle(window,(255,150,0),(ball_x,ball_y),ball_R)
     #Отрисока прямоугоника (Окно, (Цвет), (коорддинаты левого верхнего угла, ширины и высота))
     pygame.draw.rect(window,(210,40,0),(rect_x,rect_y,rect_W,rect_H))
     # Изменение положения шарика. К текущему положению прибавляется скорость по двум осям
+    pygame.draw.rect(window,(210,40,0), (bot_rect_x,bot_rect_y, rect_W, rect_H))
     ball_x+= speed_x
     ball_y+= speed_y
     # Проверка столкновений со стенками и отскок от них
+    
+
+    #Проверка столкновения шарика с ракетками игрока и бота
+    if (ball_x-ball_R <= rect_W):
+        if ball_y >= rect_y and ball_y <= rect_y+rect_H:
+            speed_x*=-1
+
+    if (ball_x + ball_R >= win_W- rect_W):
+        if ball_y >= bot_rect_y and ball_y <= bot_rect_y+rect_H:
+            speed_x*=-1
+    
+
+    #Отскок шарика от вертикальных и горизонтальных границ
     if ball_x + ball_R >= win_W or ball_x - ball_R <= 0:
         speed_x*= -1
     if ball_y + ball_R >= win_H or ball_y - ball_R <= 0:
         speed_y*= -1
+    
     # Обновление окна приложения. Выполняется в конце цикла
     pygame.display.update()
     
